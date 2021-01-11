a173132f0e1c08014b6b809ec2c607a557a698b0
javroi <javroi@alu.iesserpis.org>
2021-01-11 10:45:22 +0100

    .

---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+        private ShoppingListViewModel mViewModel;
+        private RecyclerView mList;
+        private ShoppingListAdapter mAdapter;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            ViewModelProvider.AndroidViewModelFactory factory =
+                    ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication());
+
+            mViewModel = new ViewModelProvider(this, factory)
+                    .get(ShoppingListViewModel.class);
+
+            setupList();
+
+            setupFab();
+        }
+
+        private void setupList() {
+            mList = findViewById(R.id.list);
+            mAdapter = new ShoppingListAdapter();
+            mList.setAdapter(mAdapter);
+            mViewModel.getShoppingLists().observe(this, mAdapter::setItems);
+        }
+
+        private void setupFab() {
+            findViewById(R.id.floating_action_button)
+                    .setOnClickListener(view -> addNewShoppingList());
+        }
+
+        private void addNewShoppingList() {
+            startActivity(new Intent(this, AddShoppingListActivity.class));
+        }
+    }
---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListDao.java
@@ -0,0 +1,15 @@
+package com.example.shoppinglist;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+
+import java.util.List;
+
+@Dao
+public interface ShoppingListDao {
+    @Query("SELECT * FROM shopping_list")
+    LiveData<List<ShoppingList>> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(ShoppingList shoppingList);
+}
